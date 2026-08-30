7100bcf73d4e3afa8dad0fc90f1e95f6ccb10a6c
MiLe <leducminhdhqt@gmail.com>
2026-08-31 01:16:59 +0700

    fix: swagger display

--- src/main/java/com/warehouse_kyoei/WarehouseEsApplication.java
+++ src/main/java/com/warehouse_kyoei/WarehouseEsApplication.java
@@ -2,14 +2,10 @@ package com.warehouse_kyoei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication()
 @EnableJpaAuditing
-@ComponentScan(basePackages = {"com.warehouse_kyoei"})
 public class WarehouseEsApplication {
 
 	public static void main(String[] args) {
@@ -18,10 +14,3 @@ public class WarehouseEsApplication {
 
 }
 
-@RestController
-class TestController {
-	@GetMapping("/api/ping")
-	public String ping() {
-		return "pong";
-	}
-}
--- src/main/java/com/warehouse_kyoei/common/config/OpenApiConfig.java
+++ src/main/java/com/warehouse_kyoei/common/config/OpenApiConfig.java
@@ -1,12 +1,14 @@
 package com.warehouse_kyoei.common.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 
 @Configuration
@@ -19,7 +21,21 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Warehouse API documentation")
                         .version("1.0.0")
-                        .description("API document for Warehouse management system"));
+                        .description("API document for Warehouse management system")
+                        .contact(new Contact()
+                                .name("Mile")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local development")
+                        // Add server later staging/production:
+                        // new Server().url("https://staging.warehouse.es.com").description("Staging")
+                ));
+//                .components(new Components()
+//                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+//                                .name(SECURITY_SCHEME_NAME)
+//                                .type(SecurityScheme.Type.HTTP)
+//                                .scheme("bearer")
+//                                .bearerFormat("JWT")))
+//                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
     }
 
 }
